bf74c2ab2c63ef78a046e2cbc03726fe2a362d18
Lyu Kaixie <lyukaixie@tklab136.sa.cs-private>
2017-01-23 16:09:31 +0900

    Improve the performance of keyboard

--- public/javascripts/mahjong-keyboard-control.js
+++ public/javascripts/mahjong-keyboard-control.js
@@ -102,7 +102,7 @@ function addTile(kind, target, num){
   if(target == 0){
     fuuroTemp = [];
     hand.push(tileId);
-    renewDisplay();
+    $('[name="display-0"]').append('<img src="images/mahjong/p_' + getKind(hand[i]) + 's' + getNum(hand[i]) + '_0.gif">');
   }
   else if(target == 1){
     if(fuuroTemp.length == 0){
